c5a220c67b02ed7e9d5278d5900bdd131ca32b2e
fahra akhlaq <f.akhlaq@outlook.com>
2025-03-17 14:26:54 +0000

    panel now working in sync with tabs

--- tabs.js
+++ tabs.js
@@ -4,11 +4,8 @@
 // add active class to clicked tab and panel
 
 document.addEventListener('DOMContentLoaded', function () {
-	console.log('DOM loaded');
 	const tabs = document.querySelectorAll('.tabBtn');
 	const panels = document.querySelectorAll('.tabPanel');
-	console.log(tabs);
-	console.log(panels);
 
 	tabs.forEach((tab) => {
 		tab.addEventListener('click', () => {
@@ -22,6 +19,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			});
 			// add active class to clicked tab and panel
 			tab.classList.add('active');
+			const clickedTab = tab.getAttribute('data-tab');
+			console.log(clickedTab);
+			document.getElementById(clickedTab).classList.add('active');
 		});
 	});
 });
