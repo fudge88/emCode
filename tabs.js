c2085965ae8749acdc8b3f385a5fc467664423d7
fahra akhlaq <f.akhlaq@outlook.com>
2025-03-17 13:54:11 +0000

    planned function

--- tabs.js
+++ tabs.js
@@ -1,1 +1,8 @@
-console.log('log from tabs.js');
+// get all tabs using query selector
+// add event/click to tabs
+// remove active class from all tabs and panel
+// add active class to clicked tab and panel
+
+document.addEventListener('DOMContentLoaded', function () {
+	console.log('DOM loaded');
+});
