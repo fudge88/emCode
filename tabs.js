d7329e8e6da28fee323ed10fc711787853d4c22d
fahra akhlaq <f.akhlaq@outlook.com>
2025-03-17 14:09:56 +0000

    added data and id tags to html

--- tabs.js
+++ tabs.js
@@ -5,4 +5,12 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('DOM loaded');
+	const tabs = document.querySelectorAll('.tabBtn');
+	const panels = document.querySelectorAll('.tabPanel');
+	console.log(tabs);
+	console.log(panels);
+
+	tabs.forEach((tab) => {
+		tab.addEventListener('click', () => {});
+	});
 });
